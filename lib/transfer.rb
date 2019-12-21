a33923a490fcfe5391d9753c82842a59a791074e
melia miller <meliajm@gmail.com>
2019-12-21 17:00:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end 
   
   def execute_transaction
-    binding.pry
+    # binding.pry
     # self.valid? && sender.balance >= self.amount ? sender.balance -= self.amount && receiver.balance += self.amount && @status == "complete" : @status = "rejected"
     if self.valid? && sender.balance >= self.amount
       sender.balance -= self.amount 
