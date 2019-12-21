86b8b344fe220dae06b14e9f5f3821b957a91235
melia miller <meliajm@gmail.com>
2019-12-21 17:03:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,6 +23,7 @@ class Transfer
       sender.balance -= self.amount 
       receiver.balance += self.amount
       self.status = "complete"
+      
     else
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
